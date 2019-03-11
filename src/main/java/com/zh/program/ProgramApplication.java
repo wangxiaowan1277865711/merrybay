6537db5d1b7000a65a135bfe45cd275fee3f5eb8
sdzczh <zgsdzczh@163.com>
2019-03-11 14:59:57 +0800

    add Intercaptor

--- src/main/java/com/zh/program/ProgramApplication.java
+++ src/main/java/com/zh/program/ProgramApplication.java
@@ -1,9 +1,12 @@
 package com.zh.program;
 
+import com.zh.program.Intercaptor.Intercaptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @ServletComponentScan
@@ -11,6 +14,16 @@ public class ProgramApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProgramApplication.class, args);
+    }
+
+    //mvc控制器
+    @Configuration
+    static class WebMvcConfigurer extends WebMvcConfigurerAdapter {
+        //增加拦截器
+        public void addInterceptors(InterceptorRegistry registry){
+            registry.addInterceptor(new Intercaptor())    //指定拦截器类
+                    .addPathPatterns("/*");        //指定该类拦截的url
+        }
     }
 
 }
