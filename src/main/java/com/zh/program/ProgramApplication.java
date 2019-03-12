8d3eda11c984e01dceaf1cfb5f383c14622ea883
sdzczh <zgsdzczh@163.com>
2019-03-12 09:58:09 +0800

    add scheduled

--- src/main/java/com/zh/program/ProgramApplication.java
+++ src/main/java/com/zh/program/ProgramApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @ServletComponentScan
+@EnableScheduling
 public class ProgramApplication {
 
     public static void main(String[] args) {
